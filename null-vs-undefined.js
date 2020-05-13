97eadee0de2101c02b5fec13ee449a190b1e9916
sayem254 <sayem254@gmail.com>
2020-05-13 18:07:16 +0200

    nullvsundefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+  let pakhi;
+
+  function add(num1, num2){
+      console.log(num1+num2);
+      return 
+  }
+
+function add(num1,num2){
+    console.log(num1,num2)
+}
+
+Null;(empty/not existence )
+
+let ages=[2,3,4,5];
+console.log(ages[12]);
+
+
+  
