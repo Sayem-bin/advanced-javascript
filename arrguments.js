99e3f1c1515b60cd71fa460c7905130694ea4a23
sayem254 <sayem254@gmail.com>
2020-05-15 17:15:50 +0200

    scope details

---
+++ arrguments.js
@@ -0,0 +1,9 @@
+//Array like object..(arguments);
+
+function add(n1,n2){
+    console.log([...arguments]);
+    return n1+n2+arguments[2];
+}
+
+const result = add(2, 3, 5, 7);
+console.log(result);
